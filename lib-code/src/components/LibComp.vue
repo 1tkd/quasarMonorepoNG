8b7ca6212551a35c714097bd5e0dce7daa0ab070
1tkd <64179085+1tkd@users.noreply.github.com>
2023-03-23 20:54:26 +0900

    Initial commit
    After npm init quasar
    
    create lib-code project
    use lib-code from main code

---
+++ lib-code/src/components/LibComp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>Lib component</div>
+  {{ postalCode }}:{{ address }}
+  <!-- <QBtn label="LibBtn"></QBtn> -->
+  <!-- <input v-model="address"/>--> 
+  
+  <button @click="fetchAddress">btn</button> 
+  <!-- <QBtn label="qbtn"></QBtn> -->
+</template>
+
+<script setup lang="ts">
+// import { QBtn } from 'quasar';
+import { ref } from 'vue';
+// import { Core as YubinBangoCore } from 'yubinbango-core2'
+
+const postalCode=ref('3591145')
+const address=ref('')
+
+// 郵便番号から住所を取得する関数
+function fetchAddress() {
+  // new YubinBangoCore(postalCode.value, function(addr:any) {
+  //   // region=都道府県, locality=市区町村, street=町域
+  //   const address1 = addr.region + addr.locality + addr.street
+
+  //   address.value = address1
+  // })
+}
+</script>
---
+++ main-code/src/components/ExampleComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>mainComp {{ func1() }}</p>
+    <LIbComp></LIbComp>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import LIbComp from 'lib/components/LibComp.vue'
+import {func1} from 'lib/components/utilcode'
+
+
+</script>
